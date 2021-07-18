8c276ef9ba262d7cc47d26499b4eaac37d94762a
박인호 <qrdlsgh1234@khu.ac.kr>
2021-07-18 23:37:17 +0900

    PracticeJPA

--- src/main/java/com/springboot/biz/springboot/domain/posts/PostsService.java
+++ src/main/java/com/springboot/biz/springboot/domain/posts/PostsService.java
@@ -1,6 +1,8 @@
 package com.springboot.biz.springboot.domain.posts;
 
+import com.springboot.biz.springboot.web.dto.PostsResponseDto;
 import com.springboot.biz.springboot.web.dto.PostsSaveRequestDto;
+import com.springboot.biz.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,21 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        return new PostsResponseDto(entity);
     }
 }
